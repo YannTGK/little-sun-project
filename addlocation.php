e0afd71f77511077358081998abc7d16bb11a01e
Yann Tagakou <tagakouyann@gmail.com>
2024-04-21 17:57:20 +0200

    design home af, werkende aan hub

--- addlocation.php
+++ addlocation.php
@@ -38,30 +38,77 @@ $allHubs = Hub::getAll();
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>addHub</title>
+    <link rel="stylesheet" href="styles/normalize.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/addlocation.css">
 </head>
-<a style="color: red" href="./index.php">GO HOME</a>
+
 
 <body>
-    <?php if (isset($error)) : ?>
-        <div><?php echo $error ?></div>
-    <?php endif; ?>
-    <form action="" method="post">
-        <label for="hubname">Hubname</label>
-        <input type="text" name="hubname" id="hubname" />
-        <label for="hublocation">Hublocation</label>
-        <input type="text" name="hublocation" id="hublocation" />
-        <input type="submit" value="Add hub" />
-    </form>
+    <?php include_once(__DIR__ . "/classes/nav.php"); ?>
 
-    <?php foreach ($allHubs as $hub) : ?>
-        <div>
-            <?php echo htmlspecialchars($hub['hubname']) . " " . htmlspecialchars($hub['hublocation']); ?>
-            <form action="" method="post" style="display: inline;">
-                <input type="hidden" name="delete_hub" value="<?php echo $hub['id']; ?>">
-                <input type="submit" value="Delete">
-            </form>
+    <div class="screen">
+        <div class="title">
+            <h1>Add Hub Location</h1>
+            <a class="kruis" href="./index.php"></a>
         </div>
-    <?php endforeach ?>
+        <?php if (isset($error)) : ?>
+            <div><?php echo $error ?></div>
+        <?php endif; ?>
+
+        <div class="holder">
+
+            <div class="left">
+                <form action="" method="post" class="formHolder">
+                    <div class="form">
+                        <label for="hubname">Hubname</label>
+                        <input type="text" name="hubname" id="hubname" />
+                    </div>
+                    <div class="form">
+                        <label for="hublocation">Hublocation</label>
+                        <input type="text" name="hublocation" id="hublocation" />
+                    </div>
+                    <span class="editLink">
+                    <input type="submit" value="Add hub" class="formButton"/>
+                    </span>
+                </form>
+                <div class="hubImg">
+                    
+                </div>
+
+            </div>
+
+            <form action="" method="post" class="formHolder">
+                <div class="form">
+                    <label for="hubname">Hubname</label>
+                    <input type="text" name="hubname" id="hubname" />
+                </div>
+                <div class="form">
+                    <label for="hublocation">Hublocation</label>
+                    <input type="text" name="hublocation" id="hublocation" />
+                </div>
+                <span class="editLink">
+                <input type="submit" value="Add hub" class="formButton"/>
+                </span>
+                
+            </form>
+
+            <?php foreach ($allHubs as $hub) : ?>
+                <div>
+                    <?php echo htmlspecialchars($hub['hubname']) . " " . htmlspecialchars($hub['hublocation']); ?>
+                    <form action="" method="post" style="display: inline;">
+                        <input type="hidden" name="delete_hub" value="<?php echo $hub['id']; ?>">
+                        <input type="submit" value="Delete">
+                    </form>
+                </div>
+            <?php endforeach ?>
+        </div>
+        
+       
+
+        
+    </div>
+   
 </body>
 
 </html>
