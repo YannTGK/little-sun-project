82a47a85995f4edf5164426c1d8ff0b1f8e18cd4
Yann Tagakou <tagakouyann@gmail.com>
2024-04-21 16:57:11 +0200

    design hoofdpagina af

--- index.php
+++ index.php
@@ -6,25 +6,67 @@ if(!isset($_SESSION['loggedin'])){
 }
 
 
-if(isset($_SESSION['role'])){
+/*if(isset($_SESSION['role'])){
     $role = $_SESSION['role'];
     echo "Rol: " . $role;
 } else {
     echo "Rol niet gevonden in sessie.";
-}
+}*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="styles/normalize.css">
+  <link rel="stylesheet" href="styles/style.css">
+  <link rel="stylesheet" href="styles/home.css">
   <title>Littlesun</title>
-  <a style= "color:red" href="./addlocation.php">add location of hub</a>
-  <a style= "color:red" href="./addpersonal.php">add a personal</a>
 </head>
 <body>
-<div class="header">
-    <?php include_once("../Littlesun/classes/nav.php"); ?>
-</div>  
+    <?php include_once(__DIR__ . "/classes/nav.php"); ?>
+
+    <div class="screen">
+        <h1> 
+            <?php if(isset($_SESSION['role'])){
+            $role = $_SESSION['role'];
+            echo $role;
+            } else {
+            echo "Rol niet gevonden in sessie.";
+            } ?> pannel
+        </h1>
+        
+        <div class="holder">
+            <div class="articles">
+                <div class="article">
+                    <h2>Add Hub Location</h2>
+                    <p>Expand your agricultural empire with ease! With this menu, you can effortlessly add and remove locations, allowing farmers to work anywhere the fertile soil calls them. </p>
+                    <span class="editLinks">
+                        <a class="YButton" href="./addlocation.php">Edit</a>
+                    </span>
+                    
+                </div>
+                <div class="article">
+                    <h2>Add Pesonel</h2>
+                    <p>Empower your workforce management effortlessly! With our menu, you can seamlessly add and remove personnel, ensuring your team is optimized wherever the job takes them.</p>
+                    <span class="editLinks">
+                        <a class="YButton" href="./addpersonal.php">Edit</a>
+                    </span>
+                  
+                </div>
+
+
+            </div>
+
+            <div class="homeImg">
+                &nbsp;
+            </div>
+           
+       
+            
+        </div>
+        
+
+    </div>
+
 </body>
 </html>
