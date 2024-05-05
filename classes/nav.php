8ad02f2d98f359a43c12af52f84ddd68abd292f2
Yann Tagakou <tagakouyann@gmail.com>
2024-05-05 21:12:55 +0200

    design done deliverable 2

--- classes/nav.php
+++ classes/nav.php
@@ -8,22 +8,22 @@
                 </a>
                 
                 <a href="index.php" class="navItems homeActive">
-                        <div class="home img"><img src="../images/HomeIcon.svg" alt=""></div>
+                        <div class="home img"></div>
                         <p class="navTxt">Home</p>
                 </a>
 
                 <a href="profile.php" class="navItems profileActive">
-                        <div class="profile img"><img src="../images/HomeIcon.svg" alt=""></div>
+                        <div class="profile img"></div>
                         <p class="navTxt">Profile</p>
                 </a>
 
                 <a href="calendar.php" class="navItems calendarActive">
-                        <div class="calendar img"><img src="../images/HomeIcon.svg" alt=""></div>
+                        <div class="calendar img"></div>
                         <p class="navTxt">calendar</p>
                 </a>
 
                 <a href="settings.php" class="navItems settingsActive">
-                        <div class="settings img"><img src="../images/HomeIcon.svg" alt=""></div>
+                        <div class="settings img"></div>
                         <p class="navTxt">settings</p>
                 </a>   
 
