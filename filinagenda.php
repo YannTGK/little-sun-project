494e78d39307c01e7b438e9c2c5370f790237a10
Yann Tagakou <tagakouyann@gmail.com>
2024-05-17 19:19:16 +0200

    not good

--- filinagenda.php
+++ filinagenda.php
@@ -29,7 +29,6 @@ function fetchAssignedTasks($pdo) {
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-
 // Haal de gebruikersnaam van de ingelogde gebruiker uit de sessie
 $username = isset($_SESSION['username']) ? $_SESSION['username'] : null;
 
